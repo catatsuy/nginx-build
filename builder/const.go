d0f05e0cf49dc1c4c2061efde506ef75e9921bc0
catatsuy <catatsuy@catatsuy.org>
2023-12-10 09:14:33 +0900

    Update PCRE2 and OpenSSL versions in builder/const.go

--- builder/const.go
+++ builder/const.go
@@ -9,7 +9,6 @@ const (
 
 // pcre
 const (
-	// datasource=github-tags depName=PCRE2Project/pcre2 versioning="regex:^pcre2-(?<compatibility>.*)-v?(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)?$"
 	PcreVersion           = "10.40"
 	PcreDownloadURLPrefix = "https://github.com/PhilipHazel/pcre2/releases/download"
 )
